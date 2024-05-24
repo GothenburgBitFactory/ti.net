90322ae421f231f5b58b3812258127eb7c1a7753
Matt Smida <matt@mattmail.org>
2024-05-24 14:42:50 -0600

    Troubleshooting how to use json.load / json.loads

--- bin/update-docs.py
+++ bin/update-docs.py
@@ -9,12 +9,13 @@ import re
 # SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 # DOC_PATH = os.path.join(SCRIPT_DIR, "doc-changes.json")
 DOC_PATH = "/tmp/venv/bin/doc-changes.json"  # TODO: Eliminate hard-code
+# DOC_PATH = "test-doc-changes.json"  # TODO: Eliminate hard-code
 
 
 def update_files():
     with open(DOC_PATH) as f:
-        print(f.read())   # TODO: Eliminate print
-        doc_changes = json.load(f)
+        # print(f.read())   # TODO: Eliminate print
+        doc_changes = json.loads(f.read())
 
     pattern = re.compile(r'doc/man\d/(.*)')
     filenames = [pattern.sub(r'\1', name)
